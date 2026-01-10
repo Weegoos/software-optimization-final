16529048ec22bf8674f243d5285f43b57e3ebc57
Weegoos <batyr.ashim05@mail.ru>
2026-01-10 16:29:38 +0100

    Software Optimisation Project: The server has been updated

---
+++ final_server/src/users/dto/user.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'Batyr',
+    description: 'first name for registration',
+  })
+  firstName: string;
+
+  @ApiProperty({
+    example: 'Ashim',
+    description: 'last name for registration',
+  })
+  lastName: string;
+
+  @ApiProperty({
+    example: 'example@example.com',
+    description: 'email for registration',
+  })
+  email: string;
+
+  @ApiProperty({
+    example: '123456',
+    description: 'password for registration',
+  })
+  password: string;
+}
