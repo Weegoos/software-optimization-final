8e9a460eefde9bc59ffaa92a520ffa92621ff650
Weegoos <batyr.ashim05@mail.ru>
2026-01-11 21:20:41 +0100

    Software Optimisation: The IndexPage.vue has been updated

---
+++ final_client/src/composables/api-method/get.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { Cookies } from 'quasar'
+// import { errorMessage } from '../notify/errorMessage'
+
+export async function getMethod(serverURL, url, $q) {
+  try {
+    const response = await axios.get(`${serverURL}${url}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      withCredentials: true,
+    })
+
+    return response.data
+  } catch (error) {
+    const status = error.response?.status
+    const message = error.response?.data?.message || 'Неизвестная ошибка'
+
+    if (status === 401) {
+      console.warn('Пользователь не авторизован.')
+      Cookies.remove('access_token')
+      return null
+    } else {
+      console.error('Ошибка:', message)
+      // errorMessage($q, `Ошибка: ${message}`)
+    }
+  } finally {
+    $q.loading.hide()
+  }
+}
